ff9321000e58c2e70f5209e3ddfda362897494e2
Karishma <Karishma@LAPTOP-48ORBJJK>
2024-07-31 18:48:52 +0530

    Deleted TestNg.java file

--- Automation/src/ActionsClassMethod/MoveToElementMethod.java
+++ Automation/src/ActionsClassMethod/MoveToElementMethod.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class MoveToElementMethod {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.reliancedigital.in/");
@@ -16,8 +16,8 @@ public class MoveToElementMethod {
 		driver.findElement(By.id("wzrk-cancel")).click();
 		
 		Actions action=new Actions(driver);
-		 WebElement = driver.findElement(By.xpath("//div[text()='Home Appliances']")).click();
-		action.moveToElement(ele).per
+		 WebElement ele = driver.findElement(By.xpath("//div[text()='Home Appliances']"));
+		action.moveToElement(ele).perform();
 		
 	}
 
